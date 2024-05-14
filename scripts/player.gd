fada2e29117ff78fedc7918ac2ae673adf29ef71
Mohammad Sadeghi <moh02sadeghi@gmail.com>
2024-05-14 22:16:21 +0330

    feat: Add jump sound

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,6 +8,7 @@ const JUMP_VELOCITY = -300.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var jump_audio = $JumpAudio
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,17 +18,18 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_audio.play()
 
-	# Get the input direction: -1, 0, 1
+	# Get the input direction: -1, 0, 1.
 	var direction = Input.get_axis("move_left", "move_right")
 
-	# Flip the sprite
+	# Flip the sprite.
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
 
-	# Play animation
+	# Play animation.
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle")
@@ -36,7 +38,7 @@ func _physics_process(delta):
 	else:
 		animated_sprite.play("jump")
 
-	# Apply movement
+	# Apply movement.
 	if direction:
 		velocity.x = direction * SPEED
 	else:
